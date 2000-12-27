3b6b8ba2d2ae09eb0d9c0062d438e4e6885470bd
George Schlossnagle <gschlossnagle@php.net>
2000-12-27 05:53:38 +0000

    Added license statements to all source and header files.

--- apc_rwlock.h
+++ apc_rwlock.h
@@ -1,3 +1,18 @@
+/* ==================================================================
+ * APC Cache
+ * Copyright (c) 2000 Community Connect, Inc.
+ * All rights reserved.
+ * ==================================================================
+ * This source code is made available free and without charge subject
+ * to the terms of the QPL as detailed in bundled LICENSE file, which
+ * is also available at http://apc.communityconnect.com/LICENSE.
+ * ==================================================================
+ * Daniel Cowgill <dan@mail.communityconnect.com>
+ * George Schlossnagle <george@lethargy.org>
+ * ==================================================================
+*/
+
+
 #ifndef INCLUDED_APC_RWLOCK
 #define INCLUDED_APC_RWLOCK
 
