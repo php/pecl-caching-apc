738504433b0cdd2fe6fbd3ae1f9fee398d2abf0c
George Schlossnagle <gschlossnagle@php.net>
2001-06-08 15:22:15 +0000

    moved

---
+++ apc_util.c
@@ -0,0 +1,16 @@
+/* ==================================================================
+ * APC Cache
+ * Copyright (c) 2000-2001 Community Connect, Inc.
+ * All rights reserved.
+ * ==================================================================
+ * This source code is made available free and without charge subject
+ * to the terms of the QPL as detailed in bundled LICENSE file, which
+ * is also available at http://apc.communityconnect.com/LICENSE.
+ * ==================================================================
+ * Daniel Cowgill <dan@mail.communityconnect.com>
+ * George Schlossnagle <george@lethargy.org>
+ * ==================================================================
+*/
+
+
+#include "apc_util.h"
