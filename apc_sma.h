e12b44fdc6ddb7f4f4d135b0e2cab116a3345e6e
Dan Hardiker <dan@php.net>
2001-09-20 18:39:38 +0000

    Added documentation.

--- apc_sma.h
+++ apc_sma.h
@@ -16,11 +16,28 @@
 
 /* Simple shared memory allocator. */
 
+/*
+ * apc_sma_init: Initialize the shared memory allocator. Must be called once,
+ * and only by the parent process -- before it forks any child processes.
+ */
 extern void apc_sma_init(int numseg, int segsize);
+
+/*
+ * apc_sma_cleanup: Cleans up the shared memory allocator. Should be called
+ * only once, and only by the parent process.
+ */
 extern void apc_sma_cleanup();
+
+/*
+ * apc_sma_malloc: Allocates size bytes of shared memory. (Aborts the program
+ * if insufficient resources are available to fulfill the request.)
+ */
 extern void* apc_sma_malloc(int size);
+
+/*
+ * apc_sma_free: Frees memory previously allocated by apc_sma_malloc. (Aborts
+ * the program if the supplied address was not returned by apc_sma_malloc.)
+ */
 extern void apc_sma_free(void* p);
-//extern void apc_sma_toshared(void* p, int* shmid, int* off);
-//extern void* apc_sma_tolocal(int shmid, int off);
 
 #endif
