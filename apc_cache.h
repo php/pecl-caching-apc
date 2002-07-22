b9eaa5fb849f2ad7be7eec70341869368b919a85
Dan Hardiker <dan@php.net>
2002-07-22 22:37:01 +0000

    Changed license.

--- apc_cache.h
+++ apc_cache.h
@@ -1,17 +1,21 @@
-/* ==================================================================
- * APC Cache
- * Copyright (c) 2000-2001 Community Connect, Inc.
- * All rights reserved.
- * ==================================================================
- * This source code is made available free and without charge subject
- * to the terms of the QPL as detailed in bundled LICENSE file, which
- * is also available at http://apc.communityconnect.com/LICENSE.
- * ==================================================================
- * Daniel Cowgill <dan@mail.communityconnect.com>
- * George Schlossnagle <george@lethargy.org>
- * ==================================================================
+/* 
+   +----------------------------------------------------------------------+
+   | APC
+   +----------------------------------------------------------------------+
+   | Copyright (c) 2000-2002 Community Connect Inc.
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 2.02 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available at through the world-wide-web at                           |
+   | http://www.php.net/license/2_02.txt.                                 |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Authors: Daniel Cowgill <dcowgill@communityconnect.com>              |
+   |          George Schlossnagle <george@lethargy.org>                   |
+   +----------------------------------------------------------------------+
 */
-
 
 #ifndef INCLUDED_APC_CACHE
 #define INCLUDED_APC_CACHE
