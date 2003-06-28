f842cdaa28a1dfbb01fc4fe0d8a8df0c4c3b4444
George Schlossnagle <gschlossnagle@php.net>
2003-06-28 16:43:17 +0000

    make fcntl locks default.

--- apc_lock.h
+++ apc_lock.h
@@ -20,16 +20,16 @@
 #include "apc_fcntl.h"
 #include "config.h"
 
-#ifdef APC_FCNTL_LOCKS
-#define apc_lck_create(a,b,c) apc_fcntl_create((a))
-#define apc_lck_destroy(a)    apc_fcntl_destroy(a)
-#define apc_lck_lock(a)       apc_fcntl_lock(a)
-#define apc_lck_unlock(a)     apc_fcntl_unlock(a)
-#else
+#ifdef APC_SEM_LOCKS
 #define apc_lck_create(a,b,c) apc_sem_create(NULL,(b),(c))
 #define apc_lck_destroy(a)    apc_sem_destroy(a)
 #define apc_lck_lock(a)       apc_sem_lock(a)
 #define apc_lck_unlock(a)     apc_sem_unlock(a)
+#else
+#define apc_lck_create(a,b,c) apc_fcntl_create((a))
+#define apc_lck_destroy(a)    apc_fcntl_destroy(a)
+#define apc_lck_lock(a)       apc_fcntl_lock(a)
+#define apc_lck_unlock(a)     apc_fcntl_unlock(a)
 #endif
 
 #endif
