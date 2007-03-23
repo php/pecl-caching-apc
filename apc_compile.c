70e3b6ed93b94efcba21d6c7252fd85e4af3c93c
Gopal Vijayaraghavan <gopalv@php.net>
2007-03-23 00:10:26 +0000

    Ensure that relative paths are canonicalized if experimental optimisations are enabled.

--- apc_compile.c
+++ apc_compile.c
@@ -1094,6 +1094,8 @@ zend_op_array* apc_copy_op_array(zend_op_array* dst, zend_op_array* src, apc_mal
     int local_dst_alloc = 0;
 #ifdef APC_EXPERIMENTAL_OPTIMISATIONS
     apc_fileinfo_t fileinfo;
+    char canon_path[MAXPATHLEN];
+    char *fullpath = NULL;
 #endif
 #ifdef ZEND_ENGINE_2
     apc_opflags_t * flags = NULL;
@@ -1269,11 +1271,14 @@ zend_op_array* apc_copy_op_array(zend_op_array* dst, zend_op_array* src, apc_mal
                 /* constant includes */
                 if(!IS_ABSOLUTE_PATH(Z_STRVAL_P(&zo->op1.u.constant),len)) { 
                     if (apc_search_paths(Z_STRVAL_P(&zo->op1.u.constant), PG(include_path), &fileinfo) == 0) {
-                        zend_op *dzo = &(dst->opcodes[i]);
-                        deallocate(dzo->op1.u.constant.value.str.val);
-                        dzo->op1.u.constant.value.str.len = strlen(fileinfo.fullpath);
-                        dzo->op1.u.constant.value.str.val = apc_xstrdup(fileinfo.fullpath,
-                                                                allocate);
+                        if((IS_ABSOLUTE_PATH(fileinfo.fullpath, strlen(fileinfo.fullpath)) && (fullpath = fileinfo.fullpath))
+                                || (fullpath = realpath(fileinfo.fullpath, canon_path))) {
+                            /* is either an absolute path or it goes through a realpath() */
+                            zend_op *dzo = &(dst->opcodes[i]);
+                            deallocate(dzo->op1.u.constant.value.str.val);
+                            dzo->op1.u.constant.value.str.len = strlen(fullpath);
+                            dzo->op1.u.constant.value.str.val = apc_xstrdup(fullpath, allocate);
+                        }
                     }
                 }
             }
