4e816cffe145605ca56ecf8dbd677457ad2aab15
Gopal Vijayaraghavan <gopalv@php.net>
2008-12-12 11:43:36 +0000

    fix version in php_apc.h

--- php_apc.h
+++ php_apc.h
@@ -35,7 +35,7 @@
 #include "apc_php.h"
 #include "apc_globals.h"
 
-#define PHP_APC_VERSION "3.1.1"
+#define PHP_APC_VERSION "3.112"
 
 extern zend_module_entry apc_module_entry;
 #define apc_module_ptr &apc_module_entry
