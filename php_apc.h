8dcc6c1ba5996b17ccc5ed482192863e1d06791e
Rasmus Lerdorf <rasmus@php.net>
2009-06-10 11:36:58 +0000

    -dev so we know we are running the -dev version for now

--- php_apc.h
+++ php_apc.h
@@ -35,7 +35,7 @@
 #include "apc_php.h"
 #include "apc_globals.h"
 
-#define PHP_APC_VERSION "3.1.2"
+#define PHP_APC_VERSION "3.1.3-dev"
 
 extern zend_module_entry apc_module_entry;
 #define apc_module_ptr &apc_module_entry
