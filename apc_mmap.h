2293bffde95aac7f99819f647322e8866a948c21
Kalle Sommer Nielsen <kalle@php.net>
2009-10-09 00:17:36 +0000

    MFT: Bump copyright year

--- apc_mmap.h
+++ apc_mmap.h
@@ -12,7 +12,7 @@
   | obtain it through the world-wide-web, please send a note to          |
   | license@php.net so we can mail you a copy immediately.               |
   +----------------------------------------------------------------------+
-  | Authors: Gopal V <gopalv@php.net> 
+  | Authors: Gopal V <gopalv@php.net>                                    |
   +----------------------------------------------------------------------+
 
    This software was contributed to PHP by Community Connect Inc. in 2002
