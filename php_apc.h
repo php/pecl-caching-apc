cb9a04cb46013543401f0d06782dcf5c52f42282
Rasmus Lerdorf <rasmus@php.net>
2012-08-16 16:01:08 +0000

    Back to dev

--- php_apc.h
+++ php_apc.h
@@ -35,7 +35,7 @@
 #include "apc_php.h"
 #include "apc_globals.h"
 
-#define PHP_APC_VERSION "3.1.12"
+#define PHP_APC_VERSION "3.1.13-dev"
 
 extern zend_module_entry apc_module_entry;
 #define apc_module_ptr &apc_module_entry
