17631c6de9bbcaa486ac000a126ca80cd437bc68
Rasmus Lerdorf <rasmus@php.net>
2013-01-03 01:39:56 +0000

    Get ready for 3.1.14 release

--- php_apc.h
+++ php_apc.h
@@ -35,7 +35,7 @@
 #include "apc_php.h"
 #include "apc_globals.h"
 
-#define PHP_APC_VERSION "3.1.14-dev"
+#define PHP_APC_VERSION "3.1.14"
 
 extern zend_module_entry apc_module_entry;
 #define apc_module_ptr &apc_module_entry
