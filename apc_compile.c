08e2ce7ab5f59aea483d877e2bc19bb1a5bcc34f
Remi Collet <remi@php.net>
2013-01-03 07:07:45 +0000

    Fix build with latest PHP 5.5 See: http://git.php.net/?p=php-src.git;a=commitdiff;h=bc0425c02579dbbb9e9f9f38a835413c6c512eb3
    As function was previously set to NULL, I think skipping this is ok.
    
    =====================================================================
    FAILED TEST SUMMARY
    ---------------------------------------------------------------------
    Trying to exclude trait method multiple times (origin Zend/tests/traits/error_010.phpt) [tests/apc54_error_010.phpt]
    Exclude trait method [tests/apc54_error_010_2.phpt]
    =====================================================================
    -- Cette ligne, et les suivantes ci - dessous,
      seront ignor Ã ÂÃÂÃÂÃÂÃÂÃÂÃÂÃÂ©es--
    
    M    trunk/apc_compile.c

--- apc_compile.c
+++ apc_compile.c
@@ -2469,9 +2469,11 @@ zend_trait_alias* apc_copy_trait_alias(zend_trait_alias *dst, zend_trait_alias *
         dst->alias_len = src->alias_len;
     }
 
+#ifndef ZEND_ENGINE_2_5
     if (src->function) {
         CHECK(dst->function = my_copy_function(NULL, src->function, ctxt TSRMLS_CC));
     }
+#endif
 
     APC_COPY_TRAIT_METHOD(dst->trait_method, src->trait_method);
 
@@ -2506,9 +2508,11 @@ zend_trait_precedence* apc_copy_trait_precedence(zend_trait_precedence *dst, zen
     }
     memcpy(dst, src, sizeof(zend_trait_precedence));
 
+#ifndef ZEND_ENGINE_2_5
     if (src->function) {
         CHECK(dst->function = my_copy_function(NULL, src->function, ctxt TSRMLS_CC));
     } 
+#endif
 
     if (src->exclude_from_classes && *src->exclude_from_classes) {
         int i = 0, num_classes = 0;
